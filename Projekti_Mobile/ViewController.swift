f459f165b8f687b9f758c26053e4387df7548c08
Qendresa Potoku <qendresa.potoku@student.uni-pr.edu>
2024-02-22 09:29:31 +0100

    Initial Commit

---
+++ Projekti_Mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projekti_Mobile
+//
+//  Created by user253361 on 2/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
