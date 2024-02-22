5715bf41c18400ebaa9b35f5c3b4ce69bd4bb584
Qendresa Potoku <qendresa.potoku@student.uni-pr.edu>
2024-02-22 11:55:01 +0100

    Create ReminderListView

---
+++ Projekti_Mobile/View/ReminderListView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderListView.swift
+//  Projekti_Mobile
+//
+//  Created by user253361 on 2/22/24.
+//
+
+import SwiftUI
+
+struct ReminderListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderListView()
+}
