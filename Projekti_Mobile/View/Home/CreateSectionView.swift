dff9933382efd9f18fe494bd553c86305b75b75d
Qendresa-Potoku <qendresa.potoku@student.uni-pr.edu>
2024-02-27 21:00:43 +0100

    Create CreateSectionView

---
+++ Projekti_Mobile/View/Home/CreateSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateSectionView.swift
+//  Projekti_Mobile
+//
+//  Created by user253361 on 2/27/24.
+//
+
+import SwiftUI
+
+struct CreateSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateSectionView()
+}
